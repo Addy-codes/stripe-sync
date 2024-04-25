d888d4488e4ee05210425bba961fe462d28d12c7
Addy-codes <adeeb9839@gmail.com>
2024-04-25 16:50:48 +0530

    fix; create db if absent

--- app/db/connection.py
+++ app/db/connection.py
@@ -1,3 +1,5 @@
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from ..core.config import settings
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
@@ -5,6 +7,24 @@ from sqlalchemy.orm import sessionmaker
 DATABASE_URL = f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}/{settings.DB_NAME}"
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+def create_database():
+    """Create PostgreSQL database if it does not exist."""
+    conn = psycopg2.connect(dbname='postgres', user=settings.DB_USER, password=settings.DB_PASSWORD,
+                            host=settings.DB_HOST)
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT 1 FROM pg_catalog.pg_database WHERE datname = %s", (settings.DB_NAME,))
+    exists = cursor.fetchone()
+    if not exists:
+        cursor.execute(f"CREATE DATABASE {settings.DB_NAME}")
+        print(f"Database {settings.DB_NAME} created.")
+    else:
+        print(f"Database {settings.DB_NAME} already exists.")
+    
+    cursor.close()
+    conn.close()
 
 async def get_db():
     """
--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
 from app.api.api import api_router
+from .db.connection import create_database
 
 app = FastAPI(title="Zenskar", description="Two-Way Integrations")
 
 
 app.include_router(api_router)
+
+@app.on_event("startup")
+async def startup_event():
+    # Ensure the database is created at startup
+    create_database()
+    # Include other startup tasks like running Alembic migrations
+    # os.system("alembic upgrade head")
+
 
 
 @app.get("/")
